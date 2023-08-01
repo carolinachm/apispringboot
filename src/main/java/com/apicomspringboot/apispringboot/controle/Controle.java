5371c4f54623b5de80df89693f5fe1fda358e827
Carolina <carolti2013@gmail.com>
2023-08-01 17:48:51 -0300

    criando o pacote de modelo de classes

--- src/main/java/com/apicomspringboot/apispringboot/controle/Controle.java
+++ src/main/java/com/apicomspringboot/apispringboot/controle/Controle.java
@@ -1,14 +1,25 @@
 package com.apicomspringboot.apispringboot.controle;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controle {
 
+   
+
     @GetMapping("")
     public String mensagem(){
         return "Hello World";
+    }
+     @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Seja bem vindo(a) ";
+    }
+    @GetMapping("/boasVindas/{nome}")
+    public String boasVindas(@PathVariable String nome){
+        return "Seja bem vindo(a) " +  nome;
     }
     
 }
---
+++ src/main/java/com/apicomspringboot/apispringboot/modelo/Pessoa.java
@@ -0,0 +1,12 @@
+package com.apicomspringboot.apispringboot.modelo;
+
+import lombok.Data;
+
+
+@Data
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    
+}
