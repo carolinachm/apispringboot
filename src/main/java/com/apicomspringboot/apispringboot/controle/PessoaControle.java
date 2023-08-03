bb990f23fe959af19e659cc3e348e908430e8ad1
Carolina <carolti2013@gmail.com>
2023-08-03 17:12:25 -0300

    criação das busca

--- src/main/java/com/apicomspringboot/apispringboot/controle/PessoaControle.java
+++ src/main/java/com/apicomspringboot/apispringboot/controle/PessoaControle.java
@@ -3,9 +3,11 @@ package com.apicomspringboot.apispringboot.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,16 +21,67 @@ public class PessoaControle {
     private PessoaRepositorio pessoaRepositorio;
 
     @PostMapping("/api/pessoa")
-    public Pessoa cadastraPessoa(@RequestBody Pessoa pessoa){
+    public Pessoa cadastraPessoa(@RequestBody Pessoa pessoa) {
         return pessoaRepositorio.save(pessoa);
     }
+
     @GetMapping("/api/pessoa")
-    public List<Pessoa> buscarTodasPessoas(){
+    public List<Pessoa> buscarTodasPessoas() {
         return pessoaRepositorio.findAll();
     }
-     @GetMapping("/api/{codigo}")
-    public Pessoa buscarPessoaCodigo(@PathVariable Long codigo){
+
+    @GetMapping("/api/{codigo}")
+    public Pessoa buscarPessoaCodigo(@PathVariable Long codigo) {
         return pessoaRepositorio.findByCodigo(codigo);
     }
+
+    @GetMapping("/api/contador")
+    public long contador() {
+        return pessoaRepositorio.count();
+    }
+
+    @GetMapping("/api/ordenarPorNome")
+    public List<Pessoa> ordernarPorNome() {
+        return pessoaRepositorio.findByOrderByNome();
+    }
+
+    @GetMapping("/api/ordenarPorNomeDesc")
+    public List<Pessoa> ordernarPorNomeDesc() {
+        return pessoaRepositorio.findByOrderByNomeDesc();
+    }
+
+    @GetMapping("/api/nomeContem")
+    public List<Pessoa> nomeContem() {
+        return pessoaRepositorio.findByNomeContaining("T");
+    }
+
+    @GetMapping("/api/iniciaCom")
+    public List<Pessoa> iniciaCom() {
+        return pessoaRepositorio.findByNomeStartsWith("A");
+    }
+
+    @GetMapping("/api/terminaCom")
+    public List<Pessoa> terminaCom() {
+        return pessoaRepositorio.findByNomeEndsWith("1");
+    }
+    @GetMapping("/api/somaIdades")
+    public int somaIdades(){
+        return pessoaRepositorio.somaIdades();
+    }
+
+    @GetMapping("/api/idadeMaiorIgual")
+    public List<Pessoa> idadeMaiorIgual(){
+        return pessoaRepositorio.idadeMaiorIgual(18);
+    }
     
+    @PutMapping("/api")
+    public Pessoa editarPessoa(@RequestBody Pessoa pessoa) {
+        return pessoaRepositorio.save(pessoa);
+    }
+
+    @DeleteMapping("/api/{codigo}")
+    public void excluirPessoa(@PathVariable Long codigo) {
+        Pessoa pessoa = buscarPessoaCodigo(codigo);
+        pessoaRepositorio.delete(pessoa);
+    }
 }
--- src/main/java/com/apicomspringboot/apispringboot/repositorio/PessoaRepositorio.java
+++ src/main/java/com/apicomspringboot/apispringboot/repositorio/PessoaRepositorio.java
@@ -2,6 +2,7 @@ package com.apicomspringboot.apispringboot.repositorio;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
@@ -12,5 +13,22 @@ public interface PessoaRepositorio extends CrudRepository<Pessoa, Long>{
     List<Pessoa> findAll();
 
     Pessoa findByCodigo(Long codigo);
+
+    List<Pessoa> findByOrderByNome();
+
+    List<Pessoa> findByOrderByNomeDesc();
+
+    List<Pessoa> findByNomeContaining(String termo);
+    
+    List<Pessoa> findByNomeStartsWith(String termo);
+
+    List<Pessoa> findByNomeEndsWith(String termo);
+
+    @Query(value = "SELECT SUM(idade) FROM tb_pessoas", nativeQuery = true)
+    int somaIdades();
+
+    @Query(value = "SELECT * FROM tb_pessoas WHERE idade >= :idade", nativeQuery =  true)
+    List<Pessoa> idadeMaiorIgual(int idade);
+
     
 }
