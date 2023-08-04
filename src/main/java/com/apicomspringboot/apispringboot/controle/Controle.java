06b107ec188ac6c396e347e9ecfa49311daff64d
Carolina <carolti2013@gmail.com>
2023-08-04 15:52:33 -0300

    ResponseEntity, responsavel por retornar o status de uma requisicao

--- src/main/java/com/apicomspringboot/apispringboot/controle/Controle.java
+++ src/main/java/com/apicomspringboot/apispringboot/controle/Controle.java
@@ -1,5 +1,8 @@
 package com.apicomspringboot.apispringboot.controle;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +24,10 @@ public class Controle {
     public String boasVindas(@PathVariable String nome){
         return "Seja bem vindo(a) " +  nome;
     }
-
+    @GetMapping("/status")
+    public ResponseEntity<?> status(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
   
     
 }
